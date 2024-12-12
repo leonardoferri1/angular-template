35ccfe05143e360635f7d77f33c3c8772e217df1
Leonardo Ferri <leonardo.ferri@infortechms.com.br>
2024-12-12 17:26:38 -0300

    store, interceptor, state, loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingSpinnerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-template';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
